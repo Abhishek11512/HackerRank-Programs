4d7fd172bdbd39b0c10179e19d2c6a06990d70c7
Abhishek11512 <jaina4281@gmail.com>
2026-07-05 21:47:13 +0530

    Split and Join

---
+++ 17_String_Split_And_Join.py
@@ -0,0 +1,9 @@
+#split and join
+def split_and_join(line):
+    word = line.split()
+    return "-".join(word)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
