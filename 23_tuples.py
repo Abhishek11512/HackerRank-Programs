b4c4c86be937d00ce75a79819b3df181fdb46c76
Abhishek11512 <jaina4281@gmail.com>
2026-07-06 15:44:59 +0530

    tuples

---
+++ 23_tuples.py
@@ -0,0 +1,16 @@
+#tuples
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    integer_list = map(int, raw_input().split())
+    t = tuple(integer_list)
+    print hash(t)
+
+#raw_input only works with Python 2
+
+if __name__ == '__main__':
+    n = int(input())
+    t = tuple(map(int, input().split()))
+    print(hash(t))
+
+#This should be correct but just to pass in hackerRank tutorial use the above code since hackerRank uses an older version of python
